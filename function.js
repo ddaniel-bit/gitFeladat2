6570639b439053dfe5022e6250eaa1f0e46d5acf
ddaniel-bit <danko8651@gmail.com>
2024-09-12 09:43:40 +0200

    first element

---
+++ function.js
@@ -0,0 +1,7 @@
+function getFirstElement (arr){
+    //ide írd kódod
+    }
+    const numbers = [1, 2, 3];
+    const words = ["apple", "banana", "cherry"];
+    console.log(`First number: ${getFirstElement(numbers)}`);
+    console.log(`First word: ${getFirstElement(words)}`);
