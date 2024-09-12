036ed650f7f5aea27405985fc6b2b74ac033e031
ddaniel-bit <danko8651@gmail.com>
2024-09-12 09:45:49 +0200

    improvement

--- function.js
+++ function.js
@@ -1,7 +1,11 @@
-function getFirstElement (arr){
-    //ide írd kódod
-    }
-    const numbers = [1, 2, 3];
-    const words = ["apple", "banana", "cherry"];
-    console.log(`First number: ${getFirstElement(numbers)}`);
-    console.log(`First word: ${getFirstElement(words)}`);
+function getFirstElement(arr) {
+    return arr.length > 0 ? arr[0] : undefined;
+  }
+  
+  
+  const numbers = [1, 2, 3];
+  const words = ["apple", "banana", "cherry"];
+  
+  console.log(`First number: ${getFirstElement(numbers)}`);
+  console.log(`First word: ${getFirstElement(words)}`);
+  
